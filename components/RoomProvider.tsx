3bd20accea6eb036a8a4e9fe57b68d0e55792a42
saurabhdhingra <saurabh20work@gmail.com>
2024-11-07 19:25:55 +0530

    RoomProvider setup

---
+++ components/RoomProvider.tsx
@@ -0,0 +1,10 @@
+'use client';
+function RoomProvider({roomId, children} : {
+    roomId: string;
+    children: React.ReactNode;
+}) {
+  return (
+    <div>RoomProvider</div>
+  )
+}
+export default RoomProvider
